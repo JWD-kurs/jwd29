c75cafa8d664dad25b061726b547009eb4706d94
Zeljko Vukovic <zeljkov@uns.ac.rs>
2018-06-15 21:06:23 +0200

    lab2-end

---
+++ lab2-end/src/main/java/jwd/wafepa/WafepaApplication.java
@@ -0,0 +1,12 @@
+package jwd.wafepa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WafepaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WafepaApplication.class, args);
+	}
+}
